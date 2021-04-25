eb0f2db35b5b1dea8c64c44dbbff2a674bb0bf76
d4rksit3 <gonzaloroque21@gmail.com>
2021-04-25 11:43:33 -0500

    master

--- servidor.py
+++ servidor.py
@@ -163,15 +163,27 @@ class requestHandler(BaseHTTPRequestHandler):
             if self.send_response(301):
                 self.send_header('content-type', 'text/html')
                 output = ''
+                output += '''<!DOCTYPE html>
+                <html lang="en">
+                <head>
+                <meta charset="UTF-8">
+                <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+                <link href="favicon.ico" rel="shortcut icon">
+                <title>Restablecer IPCC</title>
+                </head>
+                <body>'''
                 output += '<div class="alert alert-success" role="alert">'
                 output += 'Operacion Exitosa!'
                 output += '</div>'
+                output += '</html></body>'
                 self.wfile.write(output.encode())
             time.sleep(2)
             self.send_header('Location', '/tasklist/new')
 
             self.end_headers()
-            
+
 
 
 #Server Web        
