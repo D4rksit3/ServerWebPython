897b997fe57f51ec5df3320bdf8feab421827df5
d4rksit3 <gonzaloroque21@gmail.com>
2021-04-26 13:28:54 -0500

    master

--- servidor.py
+++ servidor.py
@@ -80,8 +80,44 @@ class requestHandler(BaseHTTPRequestHandler):
             self.send_header('content-type', 'text/html')
             self.end_headers()
             output = ''
-            output += '<html><body><meta charset="UTF-8">'
+            output += '<html><head><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous"></head><body><meta charset="UTF-8">'
             output += '<h1>ULTIMA EJECUCION</h1>  '
+            output += '''
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+            '''
+
+            output += '''<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.1/dist/umd/popper.min.js" integrity="sha384-SR1sx49pcuLnqZUnnPwx6FCym0wLsk5JZuNx2bPPENzswTNFaQU1RDvt3wT4gWFG" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.min.js" integrity="sha384-j0CNLUeiqtyaRmlzUHCPZ+Gy5fQu0dQ6eZ/xAww941Ai1SxSY+0EQqNXNE6DZiVc" crossorigin="anonymous"></script>
+            '''
             output += f'{reload()}'
         
             output += '</body></html>'
@@ -101,6 +137,18 @@ class requestHandler(BaseHTTPRequestHandler):
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
     <link href="favicon.ico" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: white;
+   color: black;
+   text-align: center;
+}
+</style>
     <script>
     
     
@@ -123,7 +171,7 @@ class requestHandler(BaseHTTPRequestHandler):
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
 </svg></i></p>'''
             
-            output += '<input name="task" type="text" placeholder="Ingresa direccion IP">'
+            output += '<input name="task" maxlength="15" type="text" placeholder="Ingresa direccion IP">'
             #output += '''</br><h3>Acciones a realizar:</h3></br>'''
             
             output += '''<button type="submit" id="ipcc" value="Restablecer servicio ipcc">Restablecer servicio ipcc <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart-fill" viewBox="0 0 16 16">
@@ -138,7 +186,7 @@ class requestHandler(BaseHTTPRequestHandler):
   <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z"/>
 </svg></i></p>'''
             
-            output += '<input name="now" type="text" placeholder="Ingresa direccion IP">'
+            output += '<input name="now" maxlength="15" type="text" placeholder="Ingresa direccion IP">'
             #output += '''</br><h3>Acciones a realizar:</h3></br>'''
             
             output += '''<button type="submit" id="down" value="Reiniciar PC">Reiniciar Computadora<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-power" viewBox="0 0 16 16">
@@ -146,6 +194,14 @@ class requestHandler(BaseHTTPRequestHandler):
   <path d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"/>
 </svg></button>'''
             output += '</form></center>'
+            output += '''
+            <footer class="footer">
+            <div>
+            <span>Copyright © MDY BPO 2021</span>
+            </div>
+            </footer>
+            
+            '''
             output += '<html></body>'
 
             self.wfile.write(output.encode())
@@ -178,7 +234,7 @@ class requestHandler(BaseHTTPRequestHandler):
             subprocess.run(f"start pskill64.exe \\\\{a} -u administrador -p @C0l0n14l# -nobanner iexplore.exe ", shell=True) and subprocess.run(f"start pskill64.exe \\\\{a} -u administrador -p soporte@ -nobanner iexplore.exe ",shell=True)
             #self.end_headers()
             
-    
+            time.sleep(3)
             if self.send_response(301):
                 self.send_header('content-type', 'text/html')
                 output = ''
@@ -224,6 +280,8 @@ class requestHandler(BaseHTTPRequestHandler):
 
             
             subprocess.run(f"start psshutdown.exe \\\\{a} -u administrador  -p @C0l0n14l# -r ",shell=True) and subprocess.run(f"start psshutdown.exe \\\\{a} -u administrador -p soporte@ -r ",shell=True)
+
+            time.sleep(3)
             if self.send_response(301):
                 self.send_header('content-type', 'text/html')
                 output = ''
